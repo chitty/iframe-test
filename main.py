4ed9b2c310e39dbceed27dffff4b40ec28ce647b
Carlos Chitty <chitty.carlosj@gmail.com>
2020-05-26 11:46:14 -0400

    chore: Send all data

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
+import json
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    full_response = json.dumps(request.form.to_dict())
     token = request.form.get('Token', 'No token found!')
-    return render_template('main.html', all=request.form, token=token)
+    return render_template('main.html', all=full_response, token=token)
